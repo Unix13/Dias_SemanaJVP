f322a5b72bc72dc65be49fecb40e6d3dec4aac15
Unix13 <vivasjesus80@gmail.com>
2022-02-22 19:45:04 +0100

    Segunda version

--- Dias_Semana/src/diasSemana/DiasSemana.java
+++ Dias_Semana/src/diasSemana/DiasSemana.java
@@ -14,47 +14,58 @@ import java.util.Scanner;
 public class DiasSemana {
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
-			int n;
-			String s = null;
+			int numero
+			;
+			String solucion = null;
 // Solicitud de datos al usuario
-			System.out.println("El programa te devuelve el día de la semana seleccionando:");
-			System.out.println("1.- Lunes.");
-			System.out.println("2.- Martes.");
-			System.out.println("3.- Miércoles.");
-			System.out.println("4.- Jueves.");
-			System.out.println("5.- Viernes.");
-			System.out.println("6.- Sábado.");
-			System.out.println("7.- Domingo.");
-			System.out.println("_____________________________________________________________");
-			System.out.print("Dime un número y te digo a qué día de la semana corresponde: ");
-			n = scanner.nextInt();
+			numero = decisionDia(scanner);
 // Realizamos comparaciones y mostramos el resultado
-			switch (n) {
+			switch (numero) {
 			case 1:
-				s = "El día seleccionado es LUNES";
+				solucion = "El día seleccionado es LUNES";
 				break;
 			case 2:
-				s = "El día seleccionado es MARTES";
+				solucion = "El día seleccionado es MARTES";
 				break;
 			case 3:
-				s = "El día seleccionado es MIÉRCOLES";
+				solucion = "El día seleccionado es MIÉRCOLES";
 				break;
 			case 4:
-				s = "El día seleccionado es JUEVES";
+				solucion = "El día seleccionado es JUEVES";
 				break;
 			case 5:
-				s = "El día seleccionado es VIERNES";
+				solucion = "El día seleccionado es VIERNES";
 				break;
 			case 6:
-				s = "El día seleccionado es SÁBADO";
+				solucion = "El día seleccionado es SÁBADO";
 				break;
 			case 7:
-				s = "El día seleccionado es DOMINGO";
+				solucion = "El día seleccionado es DOMINGO";
 				break;
 			default:
-				s = "ERROR: número incorrecto.";
+				solucion = "ERROR: número incorrecto.";
 			}
-			System.out.println(s);
+			System.out.println(solucion);
 		}
+	}
+
+	/**
+	 * @param numeroDia
+	 * @return
+	 */
+	public static int decisionDia(Scanner numeroDia) {
+		int numero;
+		System.out.println("El programa te devuelve el día de la semana seleccionando:");
+		System.out.println("1.- Lunes.");
+		System.out.println("2.- Martes.");
+		System.out.println("3.- Miércoles.");
+		System.out.println("4.- Jueves.");
+		System.out.println("5.- Viernes.");
+		System.out.println("6.- Sábado.");
+		System.out.println("7.- Domingo.");
+		System.out.println("_____________________________________________________________");
+		System.out.print("Dime un número y te digo a qué día de la semana corresponde: ");
+		numero = numeroDia.nextInt();
+		return numero;
 	}
 }
